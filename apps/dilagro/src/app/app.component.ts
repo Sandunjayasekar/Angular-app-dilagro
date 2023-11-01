67b28c02aa438931b3d8d1170b660d2f5f90ae56
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2023-11-01 20:18:44 +0530

    bindings and directives

--- apps/dilagro/src/app/app.component.ts
+++ apps/dilagro/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, CommonModule],
   selector: 'seng41293-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dilagro';
+  name = 'Sandun';
+  frameworks = ['nodejs', 'springboot', 'react'];
 }
