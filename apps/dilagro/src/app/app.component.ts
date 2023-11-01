59c6abbbfd1c2122752d614cecbd0c3aec6b019f
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2023-11-01 21:22:48 +0530

    login page created

--- apps/dilagro/src/app/app.component.ts
+++ apps/dilagro/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, CommonModule],
+  imports: [NxWelcomeComponent, LoginComponent, RouterModule, CommonModule],
   selector: 'seng41293-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
